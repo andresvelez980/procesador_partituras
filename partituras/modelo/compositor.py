c6e3f03ffab0650dbdc6c9d4a8bb791df519655c
Andres felipe <andrew.vm1611@gmail.com>
2026-05-21 17:20:19 -0500

    clase ReglaTransposicion

--- partituras/modelo/compositor.py
+++ partituras/modelo/compositor.py
@@ -39,3 +39,63 @@ class ReglaTransformacion(ABC):
             for i, c in enumerate(partitura)
             if ord(c) > 127
         ]
+
+class ReglaTransposicion(ReglaTransformacion):
+
+    NOTAS = ["do", "re", "mi", "fa", "sol", "la", "si"]
+
+    def partitura_valida(self, partitura: str) -> bool:
+
+        errores = []
+
+        numeros = self.encontrar_numeros_partitura(partitura)
+
+        if numeros:
+            mensaje = ", ".join(
+                [f"{c} en posición {i}" for i, c in numeros]
+            )
+
+            errores.append(
+                ContieneNumero(mensaje)
+            )
+
+        invalidos = self.encontrar_caracteres_invalidos(partitura)
+
+        if invalidos:
+            mensaje = ", ".join(
+                [f"{c} en posición {i}" for i, c in invalidos]
+            )
+
+            errores.append(
+                ContieneCaracterInvalido(mensaje)
+            )
+
+        partitura = partitura.lower()
+
+        tokens = partitura.split()
+
+        validos = self.NOTAS + ["|", "-"]
+
+        for token in tokens:
+
+            if token not in validos:
+
+                errores.append(
+                    ContieneCaracterInvalido(
+                        f"Token inválido: {token}"
+                    )
+                )
+
+        notas = [t for t in tokens if t in self.NOTAS]
+
+        if not notas:
+            errores.append(
+                SinNotas("La partitura no contiene notas")
+            )
+        if errores:
+            raise ExceptionGroup(
+                "Errores de validación",
+                errores
+            )
+
+        return True
